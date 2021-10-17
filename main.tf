81fc6157ae2d7e5ff7812d0e4e83d616c343ce02
ion <ion@hashicorp.com>
2021-10-17 22:06:11 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "4.16.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.token
+}
+
+resource "github_repository" "repository-with-terraform-github" {
+  name        = "repo-from-terraform"
+  description = "My awesome briliant fantastic marvelous quick efficient"
+  visibility = "public"
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "token" {
+  description = "token used to access githubrepo"
+  type = string
+  default = null
+}
